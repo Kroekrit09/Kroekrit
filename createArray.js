e43e0924fa49aa5518d4c80dfab5660e6d0d58b0
Kroekrit09 <s6406021622087@email.kmutnb.ac.th>
2023-01-05 14:11:18 +0700

    array

---
+++ createArray.js
@@ -0,0 +1,10 @@
+let arrayLangth =7;
+let arr1 = [];
+let arr2 = Array(arrayLangth);
+let arr3 = ['A', false, 5]
+
+console.log(arr1.length);
+console.log(arr2.length);
+console.log(arr3.length);
+console.log(arr3[0]);
+console.log(arr2[2]);
---
+++ reduce.js
@@ -0,0 +1,7 @@
+const array0fNumbers = [1, 2, 3, 4];
+
+const sum = array0fNumbers.reduce((accmulator, currenValue) => {
+    return accmulator + currenValue;
+});
+
+console.log(sum);

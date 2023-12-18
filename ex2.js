9ade51e6ec8dbb4baa4bd17a56ed051749d5c6ea
Kroekrit Lueanloi <120086362+Kroekrit09@users.noreply.github.com>
2023-12-18 11:40:10 +0700

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,10 @@
+let furniture = ['Table', 'Chairs', 'Couch'];
+
+for (const item of furniture) {
+       for (let i = 0; i < item.length; i++) {
+        const currentLetter = item[i];
+        console.log(currentLetter);
+    }
+
+        console.log("---");
+}
